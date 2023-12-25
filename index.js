0f74228714ab25147d8ea709045b5373e1ab6d38
firdausshasan <firdausshasan28@gmail.com>
2023-12-26 00:11:02 +0800

    testing

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const swaggerSpec = swaggerJsdoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 //bcrypt
 const bcrypt = require('bcrypt');
+
 const saltRounds = 10;
 var hashed;
 //token
